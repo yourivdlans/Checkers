627fcbdeaccfc4826da585b928a201ed5298060b
Youri van der Lans <youri@itflows.nl>
2012-09-06 15:07:07 +0200

    Initial Commit

---
+++ Checkers/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Checkers
+//
+//  Created by Youri van der Lans on 9/6/12.
+//  Copyright (c) 2012 Youri van der Lans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

1a78abfe750af713e3cb4a07eebede01a93c30d0
Youri van der Lans <youri@itflows.nl>
2012-09-07 15:44:55 +0200

    Initial commit.

---
+++ Checkers/Board.h
@@ -0,0 +1,13 @@
+//
+//  Board.h
+//  Checkers
+//
+//  Created by Youri van der Lans on 9/6/12.
+//  Copyright (c) 2012 Youri van der Lans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Board : NSObject {
+
+@end
---
+++ Checkers/Board.m
@@ -0,0 +1,13 @@
+//
+//  Board.m
+//  Checkers
+//
+//  Created by Youri van der Lans on 9/6/12.
+//  Copyright (c) 2012 Youri van der Lans. All rights reserved.
+//
+
+#import "Board.h"
+
+@implementation Board
+
+@end
